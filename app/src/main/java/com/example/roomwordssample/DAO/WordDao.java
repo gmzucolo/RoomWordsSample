8790ab8ff313fd293de4927bac4d4b8322992a25
gmzucolo <gustavo.zucolo@hotmail.com>
2022-01-16 11:58:06 -0300

    create Word entity, DAO, LiveData, Room database

---
+++ app/src/main/java/com/example/roomwordssample/DAO/WordDao.java
@@ -0,0 +1,23 @@
+package com.example.roomwordssample.DAO;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.roomwordssample.model.Word;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+
+    @Insert
+    void insert(Word word);
+
+    @Query("DELETE from word_table")
+    void deleteAll();
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAllWords();
+}
---
+++ app/src/main/java/com/example/roomwordssample/database/WordRoomDatabase.java
@@ -0,0 +1,37 @@
+package com.example.roomwordssample.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.roomwordssample.DAO.WordDao;
+import com.example.roomwordssample.model.Word;
+
+@Database(entities = {Word.class}, version = 1, exportSchema = false)
+public abstract class WordRoomDatabase extends RoomDatabase {
+
+    public abstract WordDao wordDao();
+
+    private static WordRoomDatabase INSTANCE;
+
+    //Singleton
+    public static WordRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    //Create database here
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database")
+                            // Wipes and rebuilds instead of migrating
+                            // if no Migration object.
+                            // Migration is not part of this practical.
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
